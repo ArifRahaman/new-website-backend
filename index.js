bfd8257e7712c67bb044194a5f1ac833473794b7
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-10-29 08:15:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.json());
 const allowedOrigins = [
   // process.env.FRONTEND_URL || "https://frontendarif.onrender.com",
-  "https://frontendpartarif.vercel.app/",
+  "https://frontendpartarif.vercel.app",
 ];
 app.use(
   cors({
@@ -260,7 +260,7 @@ app.post(
 
     const { email } = req.body;
     const filePath = req.file.path;
-    const imageUrl = `http://localhost:3001/${filePath}`;
+    const imageUrl = `https://frontendpartarif.vercel.app/${filePath}`;
     // const imageUrl = `https://frontend-pdfchat-2.onrender.com/${filePath}`;
 
     try {
