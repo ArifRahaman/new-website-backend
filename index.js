ec29b9afc72bf0f773edf0ae79fdd5cb7597370d
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-12-23 15:52:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,8 @@ cloudinary.config({
 });
 
 // MongoDB connection URI
-mongoose.connect('mongodb+srv://arifrahaman2606:NTambC6dzWTscSn1@mernstack.emb8nvx.mongodb.net/PDF', {
+//mongodb+srv://arifrahaman2606:NTambC6dzWTscSn1@mernstack.emb8nvx.mongodb.net/PDF
+mongoose.connect(process.env.MONGO_URI, {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 });
